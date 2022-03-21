e36dfcc0be4277ea48a679e89cff7c5c381ce7c0
baitianming <dawn_bai@126.com>
2022-03-21 17:24:24 +0800

    参数化/token逻辑优化

--- test_chat_token.py
+++ test_chat_token.py
@@ -11,49 +11,80 @@ class Test_wx(object):
         token失效时间2h
         不宜多次请求token接口 只在类中运行一次
     """
-    def setup_class(self):
+    @pytest.fixture(scope='session')
+    def get_token(self):
         # 接口
-        url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
+        url = 'https://qyapi.weixin.qq.com/cgi-bin'
+        token_url = url + '/gettoken'
         # 参数
         params = {
                     "corpid": "wwc4fcb20970b14103",
                     "corpsecret": "HYCEYvE8vImVbOK0DCtpryue_AksK28639bUtnrWDM4"
         }
         # 拿到token
-        response = requests.get(url, params=params)
-        self.token = response.json()['access_token']
+        response = requests.get(token_url, params=params)
+        token = response.json()['access_token']
+
+        return url, token
 
     """
         读取成员接口
     """
-    def test_getuser(self):
-
-        url = 'https://qyapi.weixin.qq.com/cgi-bin/user/get'
-        params = {
-                'access_token': self.token,
-                'userid': 'BaiTianMing'
-        }
-        response = requests.get(url, params=params)
-        print(response.json())
+    @pytest.mark.parametrize('userid', [('BaiTianMing')])
+    @pytest.mark.skip
+    def test_getuser(self, userid, get_token):
+        user_url = get_token[0] + f'/user/get?access_token={get_token[1]}&userid={userid}'
+        response = requests.get(user_url)
+        assert '白天明' == response.json()['name']
 
     """
         创建成员
             发送数据必须为json格式
         requests.post()
             data参数传的dict类型
-            json参数传的json类型 
-                
-    """
-    def test_creat_user(self):
-        url = "https://qyapi.weixin.qq.com/cgi-bin/user/create"
-        params = {
-            "access_token": self.token,
-            # 调试参数
-            # "debug": 1
-        }
+            json参数传的json类型
 
-        response = requests.post(url, params=params, json=json.load(open('./create_user.json', 'r')))
-        print(response.json())
+    """
+    @pytest.mark.skip
+    @pytest.mark.parametrize('userid, name, department, email', [('test7', '哈哈哈ha', [1], 'test5@gzdev.com')])
+    def test_creat_user(self, userid, name, department, email, get_token):
+        create_url = get_token[0] + f'/user/create?access_token={get_token[1]}'
+        json_data = {
+            'userid': userid,
+            'name': name,
+            'department': department,
+            'email': email
+        }
+        response = requests.post(create_url, json=json_data)
+        # 断言
+        assert 'created' == response.json()['errmsg']
+
+    """
+        修改成员
+
+    """
+    @pytest.mark.skip
+    @pytest.mark.parametrize('userid, name', [('test7', '一一一')])
+    def test_update_user(self, userid, name, get_token):
+        update_url = get_token[0] + f'/user/update?access_token={get_token[1]}'
+        json_data = {
+            "userid": userid,
+            "name": name
+        }
+        response = requests.post(update_url, json=json_data)
+        # 断言
+        assert 'updated' == response.json()['errmsg']
+
+    """
+        删除成员
+
+    """
+    @pytest.mark.parametrize('userid', [('test7')])
+    @pytest.mark.skip
+    def test_delete(self, userid, get_token):
+        delete_url = get_token[0] + f'/user/delete?access_token={get_token[1]}&userid={userid}'
+        response = requests.get(delete_url)
+        assert 'deleted' == response.json()['errmsg']
 
 
 if __name__ == '__main__':
